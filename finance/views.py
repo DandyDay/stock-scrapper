c2db7f2e684a04136b89e9059534510d76fb6003
dandyday <dandyday31@gmail.com>
2021-09-14 14:57:33 +0000

    210914_2

--- finance/views.py
+++ finance/views.py
@@ -5,11 +5,9 @@ import yfinance as yf
 stock_list = ['AAPL', 'AMZN', 'FB', 'NFLX', 'NVDA', 'GOOG', '005930.KS']
 stock_list_display = ['Apple Inc.', 'Amazon', 'Facebook', 'Netflix',
                       'NVIDIA', 'ALPHABET', 'Samsung Electronics Co., Ltd.']
-
-for quote in stock_list:
-    tmp = yf.download(quote, start='2020-01-01')
-    data = tmp['Close']
-    data_High = tmp['High']
+stock_YTD = []
+stock_WER = []        # WeeklyEarningRate
+stock_DFH = []        # DeclineFromHighest
 
 
 def YTD(data):
@@ -39,7 +37,19 @@ def DeclineFromHighest(data):
     return (diff, percent)
 
 
+for quote in stock_list:
+    tmp = yf.download(quote, start='2020-01-01')
+    data = tmp['Close']
+    data_High = tmp['High']
+    stock_YTD.append(YTD(data))
+    stock_DFH.append(DeclineFromHighest(data_High))
+    stock_WER.append(WeeklyEarningRate(data))
+
+
 def index(request):
     context = {'stock_list': stock_list,
-               'stock_list_display': stock_list_display}
+               'stock_list_display': stock_list_display,
+               'stock_YTD': stock_YTD,
+               'stock_DFH': stock_DFH,
+               'stock_WER': stock_WER}
     return render(request, 'finance/index.html', context)
